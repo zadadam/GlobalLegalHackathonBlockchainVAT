4c403017c0b21f73ff483be3e1c73c67441be922
zadadam <adam.zadrozny@gmail.com>
2018-02-24 12:31:31 -0600

    NIP - czy ok

--- getVATstatus.py
+++ getVATstatus.py
@@ -2,8 +2,20 @@ import sys
 
 
 def getVATstatus(nip):
+    """
 
-    response =     
+    :param nip: string jako int
+    :return:
+    """
+
+    # TODO: kazdy podatnik za wyjatkiem tych, ktorych nip %7 ==0 jest OK
+    response = {
+        'nip' : nip,
+        'czypotarnikVAT' :
+
+    }
+
+    return response
 
 if __name__ == "__main__":
     pass
